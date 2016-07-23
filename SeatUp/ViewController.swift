bcdaebde7bad6f3ba7ad2a5c6be2676efac4c67f
Yuxi Chen <yuxi.chen@berkeley.edu>
2016-07-23 16:34:24 -0700

    implemented main UI

--- SeatUp/ViewController.swift
+++ SeatUp/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
  
         
     }
---
+++ SeatUp/findSpotViewController.swift
@@ -0,0 +1,93 @@
+//
+//  findSpotViewController.swift
+//  SeatUp
+//
+//  Created by Jack Chen on 7/23/16.
+//  Copyright © 2016 Jack Chen. All rights reserved.
+//
+
+import UIKit
+
+class findSpotViewController: UIViewController {
+    
+    var direction = "none"
+    
+    @IBOutlet weak var animationsImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        print("came into here")
+        super.viewDidLoad()
+        displayCorrectDirection("left")
+    }
+    
+    
+    
+    
+    func displayCorrectDirection(directionString: String) {
+        if directionString == "left" {
+            print("should be in here")
+            animationsImage.animationImages = [
+
+                UIImage(named: "none.png")!,
+                UIImage(named: "leftOne.png")!,
+                UIImage(named: "leftTwo.png")!,
+                UIImage(named: "leftThree.png")!
+            ]
+            
+
+            animationsImage.animationDuration = 3
+            
+            animationsImage.startAnimating()
+            print("should be running")
+
+        } else if directionString == "right" {
+            
+            animationsImage.animationImages = [
+            UIImage(named: "none.png")!,
+            UIImage(named: "rightOne.png")!,
+            UIImage(named: "rightTwo.png")!,
+            UIImage(named: "rightThree.png")!
+            ]
+            
+            
+            animationsImage.animationDuration = 3
+            animationsImage.startAnimating()
+            
+        } else if directionString == "up" {
+            animationsImage.animationImages = [
+                UIImage(named: "none.png")!,
+                UIImage(named: "upOne.png")!,
+                UIImage(named: "upTwo.png")!,
+                UIImage(named: "upThree.png")!
+                
+            ]
+            
+            
+            animationsImage.animationDuration = 3
+            animationsImage.startAnimating()
+        } else if directionString == "down" {
+            animationsImage.animationImages = [
+                UIImage(named: "none.png")!,
+                UIImage(named: "downOne.png")!,
+                UIImage(named: "downTwo.png")!,
+                UIImage(named: "downThree.png")!
+            ]
+            
+            
+            animationsImage.animationDuration = 3
+            animationsImage.startAnimating()
+        } else {
+            print("this case is for when we're in the right spot ")
+            animationsImage.stopAnimating()
+            animationsImage.image = UIImage(named: "thumbsUp.png")
+            
+        }
+        
+    }
+    @IBAction func pressedDone(sender: AnyObject) {
+        
+        displayCorrectDirection("none")
+    }
+
+}
